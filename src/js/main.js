ab8b15f2b4dc6a2ca7bae0245e20c7b56d761c1c
Peliah <pelepoupa@gmail.com>
2024-05-14 21:55:29 +0100

    fads ag

--- src/js/main.js
+++ src/js/main.js
@@ -213,71 +213,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
-    function merge(arr, l, m, r) {
-        var n1 = m - l + 1;
-        var n2 = r - m;
-
-        // Create temp arrays
-        var L = new Array(n1);
-        var R = new Array(n2);
-
-        // Copy data to temp arrays L[] and R[]
-        for (var i = 0; i < n1; i++)
-            L[i] = arr[l + i];
-        for (var j = 0; j < n2; j++)
-            R[j] = arr[m + 1 + j];
-
-        // Merge the temp arrays back into arr[l..r]
-
-        // Initial index of first subarray
-        var i = 0;
-
-        // Initial index of second subarray
-        var j = 0;
-
-        // Initial index of merged subarray
-        var k = l;
-
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        // Copy the remaining elements of
-        // L[], if there are any
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        // Copy the remaining elements of
-        // R[], if there are any
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-
-
-    function mergeSort(arr, l, r) {
-        if (l >= r) {
-            return;
-        }
-        var m = l + parseInt((r - l) / 2);
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
-
 
     generateArrayBtn.addEventListener("click", generateRandomArray);
     insertionBtn.addEventListener("click", () => interval = setInterval(() => insertionSort(array), speed));
@@ -287,5 +222,4 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.reload();
     });
     quickBtn.addEventListener("click", () => quickSort(array, 0, bars - 1))
-    mergeBtn.addEventListener("click", () => mergeSort(array, 0, bars - 1))
 });
